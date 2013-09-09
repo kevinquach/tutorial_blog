e65c1ad09af09c0bb7ef5c5a91a6089c9e8712c0
Kevin Quach <kevin.quach@gmail.com>
2013-09-09 19:15:51 -0400

    add posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ TutorialBlog::Application.routes.draw do
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :posts
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -40,7 +40,7 @@ TutorialBlog::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
