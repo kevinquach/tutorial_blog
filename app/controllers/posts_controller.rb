a399231d822260d4c54b702ad904c6503fca4327
Kevin Quach <kevin.quach@gmail.com>
2013-09-09 22:27:33 -0400

    add authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
 def new
   @post = Post.new
 end
@@ -15,7 +17,7 @@ def create
 end
 
 def edit
-  @post = Post.find(params:[:id])
+  @post = Post.find(params[:id])
 end
 
 def update
